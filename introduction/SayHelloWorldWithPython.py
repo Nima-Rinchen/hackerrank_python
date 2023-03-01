d89970f03cc62c9aa4d100fdbb9eb02ff5ad299f
Nima Rinchen <123985682+Nima-Rinchen@users.noreply.github.com>
2023-03-01 22:29:18 +0530

    Create SayHelloWorldWithPython.py

---
+++ introduction/SayHelloWorldWithPython.py
@@ -0,0 +1,12 @@
+"""
+Input Format
+You do not need to read any input in this challenge.
+
+Output Format
+Print Hello, World! to stdout.
+"""
+
+
+
+if __name__ == '__main__':
+    print("Hello, World!")
