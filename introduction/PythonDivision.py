88fd1caf36ecd7826122171752d561e0a2c95fc4
Nima Rinchen <123985682+Nima-Rinchen@users.noreply.github.com>
2023-03-12 22:17:33 +0530

    Create PythonDivision.py

---
+++ introduction/PythonDivision.py
@@ -0,0 +1,17 @@
+"""
+Input Format:
+
+The first line contains the first integer, 
+The second line contains the second integer, 
+
+Output Format:
+
+Print the output
+
+"""
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(int(a//b))
+    print(float(a/b))
