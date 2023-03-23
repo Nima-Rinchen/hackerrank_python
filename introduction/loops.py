85864ab586c2b9246e10a59730b234774d996230
Nima Rinchen <123985682+Nima-Rinchen@users.noreply.github.com>
2023-03-23 21:07:23 +0530

    Create loops.py

---
+++ introduction/loops.py
@@ -0,0 +1,13 @@
+"""
+Input Format
+The first and only line contains the integer, n
+
+Output Format
+
+Print n lines, one corresponding to each i.
+"""
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(0,n):
+        print(i**2)
